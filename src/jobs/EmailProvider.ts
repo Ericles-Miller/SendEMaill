92e2a8dad8c365a24e2e610f97fa7ca88a1dd34f
Ericles-Millere <ericlesmiller15@gmail.com>
2024-04-30 00:06:35 -0300

    feat: preprare amq and nodemailer to send email

--- src/jobs/EmailProvider.ts
+++ src/jobs/EmailProvider.ts
@@ -1,13 +1,13 @@
 import nodemailer from 'nodemailer';
 
-const transporter = nodemailer.createTransport({
+export const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 587,
-  secure: false,
   auth: {
-    user: 'seuemail@gmail.com',
-    pass: 'sua_senha'
+    user: process.env.EMAIL_LOGIN,
+    pass: process.env.EMAIL_PASSWORD,
+    clientId: process.env.OAUTH_CLIENTID,
+    clientSecret: process.env.OAUTH_CLIENT_SECRET,
+    refreshToken: process.env.OAUTH_REFRESH_TOKEN,
   }
 });
-
-export default transporter;
